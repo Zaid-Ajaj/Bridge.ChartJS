4ce10469d5d7ebad2965f82d65bac94a14a0dc7e
Zaid-Ajaj <zaid.naom@gmail.com>
2016-12-24 08:54:28 +0100

    Added display format config

---
+++ Bridge.ChartJS/ChartOptions/ScalesConfig/DisplayFormatConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace Bridge.ChartJS
+{
+    [ObjectLiteral]
+    [External]
+    public class DiplayFormatConfig
+    {
+        public string Millisecond;
+        public string Second;
+        public string Minute;
+        public string Hour;
+        public string Day;
+        public string Week;
+        public string Month;
+        public string Year;
+        public string Quarter;
+    }
+}
